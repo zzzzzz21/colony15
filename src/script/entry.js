74c25cc7a9e1888f684e49c0565cad9b86788c3e
m.kurahara <kuraharamasahiro@gmail.com>
2022-04-18 23:47:08 +0900

    fix

--- src/script/entry.js
+++ src/script/entry.js
@@ -1,4 +1,16 @@
-  // import Swiper JS
-  import Swiper from 'swiper';
-  // import Swiper styles
-  import 'swiper/css';
+'use strict';
+
+/** ページ内アンカー */
+const internalLinks = document.querySelectorAll(`a[href^="#"]`);
+internalLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    const targetId = link.hash;
+    const targetElement = document.querySelector(targetId);
+    const targetOffsetTop = window.pageYOffset + targetElement.getBoundingClientRect().top;
+    window.scrollTo({
+      top: targetOffsetTop,
+      behavior: "smooth"
+    });
+  })
+})
